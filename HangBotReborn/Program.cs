1fb7d40acb33aecb8b413ee914fbc26317a7a99f
mitutee <volman1285@gmail.com>
2017-04-04 19:10:23 +0300

    Played Sucssesfully

--- HangBotReborn/Program.cs
+++ HangBotReborn/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hanggame;
+using System;
+using System.Threading;
 
 namespace HangBotReborn
 {
@@ -6,7 +8,23 @@ namespace HangBotReborn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Hanggame.Hanggame g = new Hanggame.Hanggame(new Ch());
+            g.PlayGame();
+            Thread.Sleep(9999999);
+        }
+
+        public class Ch : IChannel
+        {
+            public string Read()
+            {
+                return Console.ReadLine();
+            }
+
+            public void WriteLine(string s)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
