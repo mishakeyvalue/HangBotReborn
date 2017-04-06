da8ee447834d66d908202591508336cfe84394db
root <root@v6694.vps.ag>
2017-04-06 06:06:07 +0000

    Hot fixezz

--- HangBotReborn/Program.cs
+++ HangBotReborn/Program.cs
@@ -1,18 +1,20 @@
 ﻿using Hanggame;
 using System;
+
 using System.Threading;
+using System.Threading.Tasks;
 using vk_dotnet;
 
 namespace HangBotReborn
 {
     class Program
     {
-        private static string _t = "";
+        private static string _t = "cb8a8c455efa18e4d57cde56e25bb66dc56a33d56eae17d9ddade834c88fe21c5e052b114d1ab89869fbe";
         private static BotClient tootee = new BotClient(_t);
         static void Main(string[] args)
         {
 
-            GameBot b = new GameBot(tootee);
+            Task.Factory.StartNew( () => {GameBot b = new GameBot(tootee);});
             while (true) {
                 Thread.Sleep(35000);
                 tootee.SendTextMessageAsync("414460724", $"Alive at {DateTime.UtcNow}");
